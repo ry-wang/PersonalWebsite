419cf752cc69804b2171da6c193752151f569451
Ryan Wang <Ryan@iMac.local>
2015-08-06 21:13:36 -0400

    Implementation of JS
    Not Complete

---
+++ Main.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+var main = function() {
+	"use strict";
+    $('.Menu-Icon').click(function() {
+        $('.Navigation').animate({
+            left: '0px'
+        }, 200);
+        
+        $('body').animate ({
+            left: '285px'
+        }, 200);
+    });
+    
+    $('.Menu-Close').click(function() {
+        $('.Navigation').animate ({
+            left: '-285px'
+        }, 200);
+        $('body').animate ({
+            left: '0px'
+        }, 200);
+    });
+};
+
+$(document).ready(main);
