9df60e0eae2378388f5383799443d1a73ed3e994
Ryan Wang <shirui.wang@hotmail.com>
2015-08-07 21:24:42 -0400

    Small updates

--- Main.js
+++ Main.js
@@ -6,6 +6,9 @@ var main = function() {
         $('.Navigation').animate({
             left: '0px'
         }, 200);
+		$('.Menu-Close').animate({
+            left: '0px'
+        }, 200);
         
         $('body').animate ({
             left: '285px'
@@ -14,6 +17,9 @@ var main = function() {
     
     $('.Menu-Close').click(function() {
         $('.Navigation').animate ({
+            left: '-285px'
+        }, 200);
+		$('.Menu-Close').animate({
             left: '-285px'
         }, 200);
         $('body').animate ({
