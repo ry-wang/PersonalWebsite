82197fcf94b8c6157268d2f12e79ebc406423fa0
Ryan Wang <shirui.wang@hotmail.com>
2015-08-10 14:10:46 -0400

    Further cosmetic improvemens

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 var main = function() {
 	"use strict";
-    $('.Menu-Icon').click(function() {
+    $('.Menu').click(function() {
         $('.Navigation').animate({
             left: '0px'
         }, 200);
         $('.body').animate ({
-            left: '285px'
+            left: '250px'
         }, 200);
     });
     
     $('.Menu-Close').click(function() {
         $('.Navigation').animate ({
-            left: '-285px'
+            left: '-250px'
         }, 200);
         $('.body').animate ({
             left: '0px'
