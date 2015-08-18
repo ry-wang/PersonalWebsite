66768d8994d8b861c339770afe386f8dbf343b27
Ryan (Shirui) Wang <shirui.wang@hotmail.com>
2015-08-17 22:57:12 -0400

    Further addition of JS

--- Contact.js
+++ Contact.js
@@ -2,6 +2,8 @@
 
 var main = function() {
 	"use strict";
+	
+	//Code for slide-in menu
     $('.Menu').click(function() {
         $('.Navigation').animate({
             left: '5px'
@@ -25,6 +27,33 @@ var main = function() {
             left: '-260px'
         }, 200);
     });
+	
+	//Code for feedback from below
+	
+	var commentLength = $('#Submission').val().length;
+	var emailLength = $('#Email').val().length;
+	
+	$('#Submission').click(function() {
+		$(this).val('');
+		$(this).addClass('disabled');
+	});
+	
+	$('#Submission').keyup(function() {
+		commentLength = $(this).val().length;
+	});
+	$('#Email').keyup(function() {
+		emailLength = $(this).val().length;
+	});
+	
+	if ((commentLength > 0) && (emailLength > 0) ){
+		$('#Submission').addClass('enabled');
+	}
+	else {
+		$('#Submission').addClass('disabled');
+	}
+		
+	
+	$('#Submission').addClass('disabled');
 };
 
 $(document).ready(main);
