2360ab7d493e2f201df6bcc3406a94d287890eac
Ryan (Shirui) Wang <shirui.wang@hotmail.com>
2015-08-23 22:21:12 -0400

    Refinemen

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ var main = function() {
 
 $(document).ready(function() {
 	"use strict";
-	$('#jumbo-description').hide().fadeIn(2000);
+	$('#jumbo-description').hide().fadeIn(2000).delay(1000);
 	$('.description').hide().fadeIn(2000);
 });
