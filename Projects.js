52a7ea650c1768d2785c7bd4f77f3555378f511d
Ryan Wang <shirui.wang@hotmail.com>
2015-12-20 20:20:46 -0500

    Further work on carousel

--- Projects.js
+++ Projects.js
@@ -2,7 +2,18 @@
 
 var main = function() {
 	"use strict";
-	
-	
+	$('#Carousel').on('slide.bs.carousel', function(ev) {
+		var id = ev.relatedTarget.id;
+		switch (id) {
+			case "1": 
+				$('.carousel-1-description').css('display', 'block');
+				$('.carousel-2-description').css('display', 'none');
+			break;
+			case "2": 
+				$('.carousel-1-description').css('display', 'none');
+				$('.carousel-2-description').css('display', 'block');
+			break;
+		}
+	});
 	$(document).ready(main);
 };
