3df5ad979f1f5db84121ee17de920aeb9e3fdc23
Ryan Wang <shirui.wang@hotmail.com>
2015-12-29 21:11:13 -0500

    Added fade in to home page

---
+++ index.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	"use strict";
+	
+	$('.cover-heading').animate({opacity:1}, 2000);
+	$('.Subtitle').delay(1000).animate({opacity:1}, 2000);
+	$('.lead').delay(2000).animate({opacity:1}, 2000);
+});
