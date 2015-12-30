c9c826e350ef318027eb39cb7a18a3a22f4313ff
Ryan Wang <shirui.wang@hotmail.com>
2015-12-30 13:56:07 -0500

    Simplified code, also added stop on scroll

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // JavaScript Document
 
 function stopAnimation() {
+	"use strict";
+	
+	$('.cover-heading').stop(true, true);
+	
+	$('.Subtitle').stop(true, true);
+	$('.Subtitle').css('opacity', '1');
+		
+	$('.lead').stop(true, true);
+	$('.lead').css('opacity', '1');
 }
 
 $(document).ready(function() {
 	"use strict";
 	
-	$(window).click(function() {
-		$('.cover-heading').stop();
-		$('.cover-heading').css('opacity', '1');
-		
-		$('.Subtitle').stop();
-		$('.Subtitle').css('opacity', '1');
-		
-		$('.lead').stop();
-		$('.lead').css('opacity', '1');
-	});
-	
-	$('.cover-heading').delay(10).animate({opacity:1}, 2000);
+	$('.cover-heading').animate({opacity:1}, 2000);
 	$('.Subtitle').delay(1000).animate({opacity:1}, 2000);
 	$('.lead').delay(1000).animate({opacity:1}, 2000);
 	
+	
+	$(window).click(stopAnimation);
+	$(window).scroll(stopAnimation);
 });
