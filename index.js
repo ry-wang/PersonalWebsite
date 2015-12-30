a94251d4b1151d0a6956aee83db05c15dc54dca5
Ryan Wang <shirui.wang@hotmail.com>
2015-12-30 13:39:29 -0500

    Stopped fade in animation on click

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // JavaScript Document
 
+function stopAnimation() {
+}
+
 $(document).ready(function() {
 	"use strict";
 	
-	$('.cover-heading').animate({opacity:1}, 2000);
+	$(window).click(function() {
+		$('.cover-heading').stop();
+		$('.cover-heading').css('opacity', '1');
+		
+		$('.Subtitle').stop();
+		$('.Subtitle').css('opacity', '1');
+		
+		$('.lead').stop();
+		$('.lead').css('opacity', '1');
+	});
+	
+	$('.cover-heading').delay(10).animate({opacity:1}, 2000);
 	$('.Subtitle').delay(1000).animate({opacity:1}, 2000);
-	$('.lead').delay(2000).animate({opacity:1}, 2000);
+	$('.lead').delay(1000).animate({opacity:1}, 2000);
+	
 });
