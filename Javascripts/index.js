7e82c408d1a28f5002e1097d1c6d2f83c72a0da2
Ryan Wang <shirui.wang@hotmail.com>
2016-07-19 22:20:21 -0400

    Further work on refactoring home page

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -3,21 +3,21 @@
 function stopAnimation() {
 	"use strict";
 	
-	$('.cover-heading').stop(true, true);
+	$('.heading').stop(true, true);
 	
-	$('.Subtitle').stop(true, true);
-	$('.Subtitle').css('opacity', '1');
+	$('.heading-title').stop(true, true);
+	$('.heading-title').css('opacity', '1');
 		
-	$('.lead').stop(true, true);
-	$('.lead').css('opacity', '1');
+	$('.heading-description').stop(true, true);
+	$('.heading-description').css('opacity', '1');
 }
 
 $(document).ready(function() {
 	"use strict";
 	
-	$('.cover-heading').animate({opacity:1}, 2000);
-	$('.Subtitle').delay(1000).animate({opacity:1}, 2000);
-	$('.lead').delay(1000).animate({opacity:1}, 2000);
+	$('.heading').animate({opacity:1}, 2000);
+	$('.heading-title').delay(1000).animate({opacity:1}, 2000);
+	$('.heading-description').delay(1000).animate({opacity:1}, 2000);
 	
 	
 	$(window).click(stopAnimation);
